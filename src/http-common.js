ca0f77ef1d07e109ff224d7cfaa43ff899b723b5
murilloc <murilloc@gmail.com>
2020-11-11 23:32:28 -0300

    ready for production

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8087/',
+  baseURL: 'https://murillo-grades-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
